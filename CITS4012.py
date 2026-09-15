7974fa1f20c30584dfdedea8d16e5c9004b9c0e4
Etienne Unai Vinton Horn <70425212+etienneunai@users.noreply.github.com>
2026-09-15 11:57:00 +0800

    Uploaded Notebook

---
+++ CITS4012.py
@@ -0,0 +1,14 @@
+import marimo
+
+__generated_with = "0.24.0"
+app = marimo.App(width="medium", auto_download=["html"])
+
+
+@app.cell
+def _():
+    print("hi")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
